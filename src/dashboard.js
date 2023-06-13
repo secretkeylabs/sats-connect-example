97ef22b7d8c62f93821b7df759e6ef64f9838024
Abdul Haseeb <haseeb4239@gmail.com>
2023-06-13 15:01:45 +0500

    hardcoded PSBT base 64 for testing

--- src/dashboard.js
+++ src/dashboard.js
@@ -18,7 +18,7 @@ class Dashboard extends React.Component {
         purposes: ["ordinals", "payment"],
         message: "Address for receiving Ordinals",
         network: {
-          type: "Mainnet",
+          type: "Testnet",
         },
       },
       onFinish: (response) => {
@@ -39,7 +39,7 @@ class Dashboard extends React.Component {
           type: "Testnet",
         },
         message: "Sign Transaction",
-        psbtBase64: `cHNidP8BAKcCAAAAAtJVbmYvrS64adekw4rhCtbWQNNs9IhWFyNrhYIdkG5dAAAAAAD/////hNCzRVacJR32LJ/chDNUO9B0C3/ci9ZJzHIClfjHLSAAAAAAAP////8CoIYBAAAAAAAiUSCjXEwEb409zg9tZ4NJlmnPqVZaF2TYm9Q1txG7GQ/Q3dB+AQAAAAAAF6kUBE+9kGn9tJlLagtxL54ozfiuyqGHAAAAAAABASughgEAAAAAACJRIDmZV7+7TrMlgI87KFqU2MFVtCS9fmg3f4ZF8zwLgUEtARcguZB1Id24Xg5qN2IrfGhe+9yK5TozSSitvRLPIErU5xcAAQEgoIYBAAAAAAAXqRS9FdmY/QjP0cXH1/+o/144F2orn4ciAgN1Cual4w1uAxLWT+SalvUzyZpqp5eYW7Hlychubra2iEcwRAIgOHUp0YFRZXOrpz5V90PLaPDF/uhCPKLTLbEwVtA7wjsCICPkH0tjb3bS+jmqv/6R746ASxFWGcB8/N41rSHO+4cVAQEEFgAUGAo7GWfcpwS2XI7SsZEN06q8yTIAAAA=`,
+        psbtBase64: `cHNidP8BAP0iAQIAAAAFJpIWdnBrISLL3x0C9zTfHD4QD/iiSIZZDOddiOw3nfMBAAAAAP////8MOrAbm5eUufXOOXaynWnHlhC/zCm/vk8WvaJhQr/F9QAAAAAA/////97rrvjPb2BwWWIKyjbMprjqFD9NgiiUXYoQ/GWpjC9OAAAAAAD/////dZ5pwfzQv7/a7gpXAnMk0+xLYSxOdcAMNNe3BdJvMVgAAAAAAP////+qn+wx2SLcq2Pvt2oq6npPTjC1gnqmvW+a65mPlrtsBgAAAAAA/////wJADQMAAAAAACJRIBO7o4I8bJ6fIoZbe2RnDE1o1kIc1oZPEj1alO8zkFPYgDgBAAAAAAAXqRTKBbOmXl3+EzobfiEpnLQHh2kFW4cAAAAAAAEBK+CTBAAAAAAAIlEgOZlXv7tOsyWAjzsoWpTYwVW0JL1+aDd/hkXzPAuBQS0BAwSDAAAAARcguZB1Id24Xg5qN2IrfGhe+9yK5TozSSitvRLPIErU5xcAAQEr4JMEAAAAAAAiUSA5mVe/u06zJYCPOyhalNjBVbQkvX5oN3+GRfM8C4FBLQEDBIMAAAABFyC5kHUh3bheDmo3Yit8aF773IrlOjNJKK29Es8gStTnFwABASvgkwQAAAAAACJRIDmZV7+7TrMlgI87KFqU2MFVtCS9fmg3f4ZF8zwLgUEtAQMEgwAAAAEXILmQdSHduF4OajdiK3xoXvvciuU6M0korb0SzyBK1OcXAAEBK+CTBAAAAAAAIlEgOZlXv7tOsyWAjzsoWpTYwVW0JL1+aDd/hkXzPAuBQS0BAwSDAAAAARcguZB1Id24Xg5qN2IrfGhe+9yK5TozSSitvRLPIErU5xcAAQEr4JMEAAAAAAAiUSA5mVe/u06zJYCPOyhalNjBVbQkvX5oN3+GRfM8C4FBLQEDBIMAAAABFyC5kHUh3bheDmo3Yit8aF773IrlOjNJKK29Es8gStTnFwAAAA==`,
         broadcast: false,
         inputsToSign: [
           {
